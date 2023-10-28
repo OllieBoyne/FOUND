f23fb001c9220537338357ea74796c7f67121afc
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2023-10-28 11:16:45 +0100

    Test adding to synth

--- docs/slider.js
+++ docs/slider.js
@@ -1,14 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const baseImages = ["0.png", "1.png", "2.png", "3.png", "4.png"];
-  const directories = ["norm", "unc"] // base + overlays
-  
-  const nImages = baseImages.length
-  const nOverlays = 2;  // You can set n to any number
-  const overlayNames = ["Normals", "Uncertainty"]
-  
-  const imageWrapper = document.getElementById("image-wrapper");
-  
-   
+function createImageSliderSet(options) {
+  const {
+    baseDirectory, baseImages, directories, nOverlays, overlayNames,
+    imageWrapperId, radioButtonsId, sliderId 
+  } = options;
+
+  const nImages = baseImages.length;
+  const imageWrapper = document.getElementById(imageWrapperId);
+
    // Create image elements
   for (let n = 0; n < nImages; n ++){
     const container = document.createElement('div');
@@ -19,13 +17,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const src = baseImages[n];
     
     const baseImg = new Image();
-    baseImg.src = "images/itw/rgb/"+ src;
+    baseImg.src = baseDirectory + "/rgb/"+ src;
     baseImg.className = 'base';
     container.appendChild(baseImg);
 
     for (let i = 0; i < nOverlays; i++) {
       const overlayImg = new Image();
-      overlayImg.src = `images/itw/` + directories[i] + '/' + src;
+      overlayImg.src = baseDirectory + "/" + directories[i] + '/' + src;
       overlayImg.className = `overlay overlay${i}`;
       overlayImg.style.display = 'none';
       container.appendChild(overlayImg);
@@ -47,29 +45,30 @@ document.addEventListener("DOMContentLoaded", function() {
   radioButton.type = 'radio';
   radioButton.name = 'overlay';
   radioButton.value = `overlay${i}`;
-  radioButton.id = `radio${i}`;
+  radioButton.id = `${radioButtonsId}${i}`;
   const label = document.createElement('label');
-  label.htmlFor = `radio${i}`;
+  label.htmlFor = `${radioButtonsId}${i}`;
   label.innerText = overlayNames[i];
-  document.getElementById('radio-buttons').appendChild(radioButton);
-  document.getElementById('radio-buttons').appendChild(label);
+  document.getElementById(radioButtonsId).appendChild(radioButton);
+  document.getElementById(radioButtonsId).appendChild(label);
 }
   
   // set initial choice to be overlay0
 setTimeout(() => {
-  const defaultRadio = document.getElementById('radio0');
+  const defaultRadio = document.getElementById(`${radioButtonsId}0`);
   defaultRadio.checked = true;
   const event = new Event('change', { 'bubbles': true, 'cancelable': true });
   defaultRadio.dispatchEvent(event);
 }, 100);
 
   
-  const slider = document.getElementById("slider");
+  const slider = document.getElementById(sliderId);
 
 document.querySelectorAll('input[name="overlay"]').forEach((radio) => {
+
   radio.addEventListener('change', function() {
     document.querySelectorAll(`.${currentOverlayClass}`).forEach(el => el.style.display = 'none');
-    currentOverlayClass = `overlay${this.id.replace("radio", "")}`;
+    currentOverlayClass = this.value;
     document.querySelectorAll(`.${currentOverlayClass}`).forEach(el => {
       el.style.display = 'block';
       el.style.clipPath = `inset(0 ${100 - slider.value}% 0 0)`; // Update this line
@@ -87,5 +86,31 @@ document.querySelectorAll('input[name="overlay"]').forEach((radio) => {
     el.style.left = `${value}%`;
   });
   
+  });
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  createImageSliderSet({
+    baseDirectory: "images/itw",
+    baseImages: ["0.png", "1.png", "2.png", "3.png", "4.png"],
+    directories: ["norm", "unc"],
+    nOverlays: 2,
+    overlayNames: ["Normals", "Uncertainty"],
+    imageWrapperId: "image-wrapper-normals",
+    radioButtonsId: "radio-buttons-normals",
+    sliderId: "slider-normals"
+  });
+
+  createImageSliderSet({
+    baseDirectory: "images/synth",
+    baseImages: ["0.png", "1.png", "2.png", "3.png", "4.png"],
+    directories: ["mask", "norm", "keypoints"],
+    nOverlays: 3,
+    overlayNames: ["Mask", "Normals", "Keypoints"],
+    imageWrapperId: "image-wrapper-synth",
+    radioButtonsId: "radio-buttons-synth",
+    sliderId: "slider-synth"
   });
 });
